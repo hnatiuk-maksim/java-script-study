09d4f82211f5de9967f1e1e229bf02e0650704bc
hnatiuk-maksim <maximgnat24@gmail.com>
2022-12-25 09:31:50 +0200

    script-01

---
+++ js/script-02.js
@@ -0,0 +1,4 @@
+let elementWidth = '120px';
+const result = Number.parseInt(elementWidth);
+console.log(result);
+console.log(typeof result);
---
+++ js/variablea-script-01.js
@@ -0,0 +1,17 @@
+const age = 30;
+const totalPrice = 256.48;
+const userName = 'Manowar';
+const message = 'Добрий день!';
+const isOpen = true;
+const shouldConfirm = false;
+console.log (age);
+
+const type = typeof userName;
+console.log(type);
+
+
+const shouldRenew = confirm('Do you want to receive our messages?');
+console.log(shouldRenew);
+const answer = typeof(shouldRenew);
+console.log(answer);
+
